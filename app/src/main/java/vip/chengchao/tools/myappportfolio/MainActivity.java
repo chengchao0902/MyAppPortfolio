099443400643d6c2f27ac37010a931c9553decb1
chengchao <chengchao0902@gmail.com>
2016-06-26 00:14:07 +0800

    project 0 : complete

--- app/src/main/java/vip/chengchao/tools/myappportfolio/MainActivity.java
+++ app/src/main/java/vip/chengchao/tools/myappportfolio/MainActivity.java
@@ -2,6 +2,9 @@ package vip.chengchao.tools.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showClickToast(View view) {
+        Toast.makeText(this, ((Button) view).getText(), Toast.LENGTH_SHORT).show();
     }
 }
